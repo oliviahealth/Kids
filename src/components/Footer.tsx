afe38ad5d16c476c37debfb30c7485b9e5de66e1
Pratik Desai <pratikdesai399@gmail.com>
2024-06-02 17:29:44 +0530

    Footer Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex min-h-60 bg-[#5D1B2A] text-white text-[.75rem] md:text-sm items-center">
-      <div className="flex mx-auto">
-        <a href="https://oliviahealth.org/" target="_blank">
-          © 2024 OliviaHealth
-        </a>
+    <div className="grid grid-rows-2">
+      <div style={{ paddingTop: "4.25rem", paddingLeft: "3.25rem" }}>
+        <img src="/images/olivia-health-logo.png" alt="Olivia Health Logo"/>
+        <div style={{ marginTop: "1rem" }}>
+          <Link href={"/about"} style={{ color: "#A155B9" }} className="hover:text-purple-500 hover:underline transition duration-300">
+            About
+          </Link>
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <Link href={"/resources"} style={{ color: "#A155B9" }} className="hover:text-purple-500 hover:underline transition duration-300">
+            Resources
+          </Link>
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <Link href={"/contact-us"} style={{ color: "#A155B9" }} className="hover:text-purple-500 hover:underline transition duration-300">
+            Contact Us
+          </Link>
+        </div>
+      </div>
+      <div className="flex justify-center items-end">
+        <img src="/images/Sponsors.png" />
       </div>
     </div>
   );
