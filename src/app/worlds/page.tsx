6463ad7a7d012980f43b9c80979787dab6263cc1
AndrewChian2 <123596978+AndrewChian2@users.noreply.github.com>
2024-06-01 23:33:25 -0500

    integrated basic navbar design off the figma

--- src/app/worlds/page.tsx
+++ src/app/worlds/page.tsx
@@ -4,7 +4,7 @@ const Dashboard: React.FC = () => {
     return (
         <div className="w-full h-full flex justify-center items-center">
             <div>
-                <h1 className="font-semibold">Dashboard</h1>
+                <h1 className="font-semibold">Worlds</h1>
             </div>
         </div>
     )
