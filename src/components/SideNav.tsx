7a6799d30934232c78e92ad6e03dd04ce7e19bdd
Pratik Desai <pratikdesai399@gmail.com>
2024-06-06 20:30:56 +0530

    Image Priority

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -16,7 +16,7 @@ const SideNav: React.FC = () => {
   };
 
   return (
-    <div className="h-screen w-1/5 flex flex-col justify-start ">
+    <div className="h-screen w-1/5 flex flex-col justify-start overflow-auto">
       <LogoSection />
       <UserSection />
       <MainContent
@@ -43,6 +43,18 @@ const UserSection: React.FC = () => (
   </div>
 );
 
+const PioneerPrincessSection: React.FC = () => (
+  <div className="flex justify-center my-2">
+    <Image
+      src="/images/Pioneer Princess.png"
+      alt="Saly"
+      width={200}
+      height={400}
+      priority
+    />
+  </div>
+);
+
 interface MainContentProps {
   languages: string[];
   selectedLanguage: string;
@@ -59,15 +71,7 @@ const MainContent: React.FC<MainContentProps> = ({
   <div className="grid grid-cols-7 mt-10">
     <div className="col-start-1 col-span-6 px-6">
       <Image src="/images/Saly-17.png" alt="Saly" width={400} height={400} priority/>
-      <div className="flex justify-center my-2">
-        <Image
-          src="/images/Pioneer Princess.png"
-          alt="Saly"
-          width={200}
-          height={400}
-          priority
-        />
-      </div>
+      <PioneerPrincessSection />
       <NavigationLinks />
       {/* <Dropdown
         label="Language"
@@ -101,54 +105,11 @@ const NavigationLinks: React.FC = () => (
   </div>
 );
 
-const LatestProgress: React.FC = () => (
-  <div className="grid grid-cols-1 gap-2 mt-5">
-    <span className="font-bold text-xl">Latest Progress</span>
-    <span style={{ color: "#FF8C8C" }}>Math Mansion - Misty Plain</span>
-    <div className="grid grid-cols-2">
-      <Image src="/images/Group 42.png" alt="Medal" width={150} height={50} />
-      <div className="grid items-center justify-center">Stars</div>
-    </div>
-  </div>
-);
-
 const EllipsesColumn: React.FC<{ ellipseImages: string[] }> = ({ ellipseImages }) => (
   <div className="col-start-7">
     <div className="grid grid-cols-1 gap-3">
       {ellipseImages.map((src, index) => (
         <Image key={index} src={src} alt="Ellipse" width={40} height={35} />
-      ))}
-    </div>
-  </div>
-);
-
-const StickerBoardSection: React.FC = () => (
-  <div className="mt-10">
-    <div className="flex flex-row gap-2 items-center ml-6 mb-2">
-      <span className="font-bold text-xl">Sticker Board</span>
-    </div>
-    <div className="grid grid-cols-7">
-      <div className="col-start-1 col-span-6 px-6">
-        <div className="p-6 rounded-3xl shadow-md w-full h-full flex flex-col" style={{border: "2px solid #FF8C8C", backgroundColor: "#FFB9B9"}}>
-          <div className="text-center font-bold text-white">Misty Plain</div>
-          <div className="flex items-center justify-center mt-4">Stars</div>
-        </div>
-      </div>
-      <StickerCardsColumn />
-    </div>
-  </div>
-);
-
-const StickerCardsColumn: React.FC = () => (
-  <div className="col-start-7">
-    <div className="grid grid-cols-1 gap-3">
-      {["orange-300", "blue-300", "green-300", "purple-300"].map((bgColor, index) => (
-        <div
-          key={index}
-          className={`p-6 bg-${bgColor} rounded-lg shadow-md h-full w-full flex items-center justify-center`}
-        >
-          {index + 1}
-        </div>
       ))}
     </div>
   </div>
