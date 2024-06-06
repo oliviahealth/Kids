8458739d092fe656ba03feea5c71cd6ec58aa824
Pratik Desai <pratikdesai399@gmail.com>
2024-06-06 16:03:03 +0530

    Updated UI

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,22 +1,19 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 
 const SideNav = () => {
   const [selectedLanguage, setSelectedLanguage] = useState<string>("");
-  const [selectedYearLevel, setSelectedYearLevel] = useState<string>("");
 
   const languages = ["English", "Spanish", "French"];
-  const yearLevels = ["1", "2", "3", "4", "5"];
   const ellipseImages = Array(4).fill("/images/Ellipse 44.png");
 
-  const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleLanguageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     setSelectedLanguage(event.target.value);
-  };
-
-  const handleYearLevelChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedYearLevel(event.target.value);
   };
 
   return (
@@ -62,35 +59,75 @@ const SideNav = () => {
             />
           </div>
 
-          <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-2 mt-5">
-            <div className="p-6 bg-orange-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
-              {/* Card content */}1
-            </div>
-            <div className="p-6 bg-blue-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
-              {/* Card content */}2
-            </div>
-            <div className="p-6 bg-green-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
-              {/* Card content */}3
-            </div>
-            <div className="p-6 bg-purple-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
-              {/* Card content */}4
-            </div>
+          <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-2 mt-5 text-white font-bold">
+            <Link
+              href={"/home"}
+              className="flex items-center justify-center text-white font-bold rounded-xl px-6 py-6"
+              style={{
+                backgroundColor: "#ec9bfc",
+                border: "5px solid #e466fc",
+              }}
+            >
+              Home
+            </Link>
+
+            <Link
+              href={"/lessons"}
+              className="flex items-center justify-center text-white font-bold rounded-xl px-6 py-6"
+              style={{
+                backgroundColor: "#8be9b0",
+                border: "5px solid #4ce49c",
+              }}
+            >
+              Lessons
+            </Link>
+
+            <Link
+              href={"/activities"}
+              className="flex items-center justify-center text-white font-bold rounded-xl px-6 py-6"
+              style={{
+                backgroundColor: "#94ece2",
+                border: "5px solid #51e5d5",
+              }}
+            >
+              Activities
+            </Link>
+
+            <Link
+              href={"/worlds"}
+              className="flex items-center justify-center text-white font-bold rounded-xl px-6 py-6"
+              style={{
+                backgroundColor: "#fbb355",
+                border: "5px solid #f4946c",
+              }}
+            >
+              Worlds
+            </Link>
           </div>
 
           <div>
-          <Dropdown
-            label="Language"
-            options={languages}
-            selectedValue={selectedLanguage}
-            handleChange={handleLanguageChange}
-          />
+            <Dropdown
+              label="Language"
+              options={languages}
+              selectedValue={selectedLanguage}
+              handleChange={handleLanguageChange}
+            />
+          </div>
 
-          <Dropdown
-            label="Year Level"
-            options={yearLevels}
-            selectedValue={selectedYearLevel}
-            handleChange={handleYearLevelChange}
-          />
+          <div className="grid grid-cols-1 gap-2 mt-5">
+            <span className="font-bold text-xl">Latest Progress</span>
+            <span style={{ color: "#FF8C8C" }}>Math Mansion - Misty Plain</span>
+            <div className="grid grid-cols-2">
+              <div>
+                <Image
+                  src="/images/Group 42.png"
+                  alt="Medal"
+                  width={150}
+                  height={50}
+                />
+              </div>
+              <div className="grid items-center justify-center">Stars</div>
+            </div>
           </div>
         </div>
 
@@ -105,6 +142,39 @@ const SideNav = () => {
                 height={35}
               />
             ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <div className="flex flex-row gap-2 items-center ml-6 mb-2">
+          <span className="font-bold text-xl">Sticker Board</span>
+        </div>
+
+        <div className="grid grid-cols-7">
+          <div className="col-start-1 col-span-6 px-6">
+            <div className="p-6 border-2 border-pink-700 border-solid bg-pink-300 rounded-3xl shadow-md w-full h-full flex flex-col">
+              {" "}
+              <div className="text-center font-bold text-white">Misty Plain</div>
+              <div className="flex items-center justify-center mt-4">Stars</div>
+            </div>
+          </div>
+
+          <div className="col-start-7">
+            <div className="grid grid-cols-1 gap-3">
+              <div className="p-6 bg-orange-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
+                {/* Card content */}1
+              </div>
+              <div className="p-6 bg-blue-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
+                {/* Card content */}2
+              </div>
+              <div className="p-6 bg-green-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
+                {/* Card content */}3
+              </div>
+              <div className="p-6 bg-purple-300 rounded-lg shadow-md h-full w-full flex items-center justify-center">
+                {/* Card content */}4
+              </div>
+            </div>
           </div>
         </div>
       </div>
