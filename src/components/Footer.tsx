4e247ade1b3dff3fcfca5384b1950513421cb91d
ayfatoye <afatoye@tamu.edu>
2024-06-10 16:26:42 -0500

    updated the footer to make it horizontal and mobile responsive.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
-import Sponsors from "../../public/images/Sponsors.png"
-import oliviaLogo from "../../public/images/olivia-health-logo.png"
-
+import Sponsors from "../../public/images/Sponsors.png";
+import oliviaLogo from "../../public/images/olivia-health-logo.png";
 
 const Footer: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-5 gap-5">
-      <div className="flex flex-col items-center space-y-4 gap-2">
+      <div className="flex flex-col md:flex-row items-center justify-between w-10/12 px-10 space-x-0 md:space-x-2">
         <Image src={oliviaLogo} alt="Olivia Logo" />
-        <Link href="/about" className="!mt-0">
-          <span className="text-xl text-[#A155B9] hover:text-purple-500 hover:underline transition duration-300">About</span>
+        <Link href="/about" className="mt-2 md:mt-0">
+          <span className="text-xl text-[#A155B9] hover:text-purple-500 hover:underline transition duration-300">
+            About
+          </span>
         </Link>
-        <Link href="/resources" className="!mt-0">
-          <span className="text-xl text-[#A155B9] hover:text-purple-500 hover:underline transition duration-300">Resources</span>
+        <Link href="/resources" className="mt-2 md:mt-0">
+          <span className="text-xl text-[#A155B9] hover:text-purple-500 hover:underline transition duration-300">
+            Resources
+          </span>
         </Link>
-        <Link href="/contact-us" className="!mt-0">
-          <span className="text-xl text-[#A155B9] hover:text-purple-500 hover:underline transition duration-300">Contact Us</span>
+        <Link href="/contact-us" className="mt-2 md:mt-0 whitespace-nowrap">
+          <span className="text-xl text-[#A155B9] hover:text-purple-500 hover:underline transition duration-300">
+            Contact Us
+          </span>
         </Link>
       </div>
       <div className="flex justify-center items-center">
