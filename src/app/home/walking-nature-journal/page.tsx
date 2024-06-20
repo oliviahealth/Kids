a503d98048c0b9907eca98d9eb0a8a3827d827b4
Sebastian Oberg <Sebastianoberg2002@gmail.com>
2024-06-20 14:02:47 -0500

    Updated directory information

---
+++ src/app/home/walking-nature-journal/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './WalkingNatureJournal.module.css';
+
+const WalkingNatureJournal = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}></main>
+    </div>
+  );
+}
+
+export default WalkingNatureJournal;
