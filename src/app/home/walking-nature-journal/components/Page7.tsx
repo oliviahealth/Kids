553e42d76f6daa5050a38598bd0260ecd18bc44e
Sebastian Oberg <Sebastianoberg2002@gmail.com>
2024-07-01 16:18:07 -0500

    Partial implementation of walking activity

---
+++ src/app/home/walking-nature-journal/components/Page7.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const WalkingActivityPage7: React.FC<{
+  onNext: () => void;
+  onBack: () => void;
+}> = ({ onNext, onBack }) => {
+  return (
+    <h1>
+      Third Screen
+    </h1>
+  );
+}
+
+export default WalkingActivityPage7;
