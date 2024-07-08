088a8806401bfa210da92d432e33c253d8f68646
Pratik Desai <pratikdesai399@gmail.com>
2024-07-08 00:13:06 -0500

    Activities full screen

--- src/app/adventure-bay/marker2/page.tsx
+++ src/app/adventure-bay/marker2/page.tsx
@@ -50,17 +50,12 @@ const AdventureBayActivity: React.FC = () => {
     children,
   }) => (
     <div className="relative w-full h-full max-h-screen overflow-hidden">
-      <div className="z-5 absolute inset-0">
-        <Image
-          src={tempBackground}
-          layout="fill"
-          objectFit="cover"
-          alt="Background Image"
-        />
-      </div>
-      <div className="grid grid-cols-12 grid-rows-12 h-full w-full">
-        <div className="row-start-2 row-end-12 col-start-2 col-end-12 z-20 h-full overflow-y-auto">
-          <div className="relative shadow-2xl rounded-2xl h-full p-10" style={{backgroundColor: "#E1FF8D"}}>
+      <div className="h-full w-full">
+        <div className="z-20 h-full overflow-y-auto">
+          <div
+            className="relative shadow-2xl rounded-2xl h-full p-10"
+            style={{ backgroundColor: "#E1FF8D" }}
+          >
             {children}
           </div>
         </div>
@@ -103,19 +98,22 @@ const AdventureBayActivity: React.FC = () => {
           <ActivityPage2 onNext={handleNext} onBack={handleBack} />
         </ActivityContainer>
       ),
-    },{
+    },
+    {
       content: (
         <ActivityContainer>
           <ActivityPage3 onNext={handleNext} onBack={handleBack} />
         </ActivityContainer>
       ),
-    },{
+    },
+    {
       content: (
         <ActivityContainer>
           <ActivityPage4 onNext={handleNext} onBack={handleBack} />
         </ActivityContainer>
       ),
-    },{
+    },
+    {
       content: (
         <ActivityContainer>
           <ActivityPage5 onNext={handleNext} onBack={handleBack} />
