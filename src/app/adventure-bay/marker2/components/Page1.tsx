a30e71dc498060976d66822a58e3148c77546df2
Pratik Desai <pratikdesai399@gmail.com>
2024-07-08 20:13:25 -0500

    Text Justify for Introduction

--- src/app/adventure-bay/marker2/components/Page1.tsx
+++ src/app/adventure-bay/marker2/components/Page1.tsx
@@ -73,7 +73,7 @@ export default function Page1({
       <div className="h-4/6 w-full flex gap-4">
         <div className="w-1/2 flex flex-col gap-4">
           <h1 className="text-xl font-bold">Introduction</h1>
-          <p className="text-xs 2xl:text-lg">
+          <p className="text-xs 2xl:text-lg text-justify">
             Coloring is known to be a favorite activity among preschoolers, as
             it allows for creativity, fun, and the ability for the children to
             express themselves. However, coloring is not just an activity for
