ea526397346052fef7c105f1310fe4f46beb71a4
Sebastian Oberg <Sebastianoberg2002@gmail.com>
2024-07-09 16:19:46 -0500

    Added activity card

--- src/app/home/pregnancy/relaxation-yoga/components/Page2.tsx
+++ src/app/home/pregnancy/relaxation-yoga/components/Page2.tsx
@@ -28,43 +28,77 @@ const RelaxationYogaPage2: React.FC<{
           <div className={styles.activityList}>
             <div className={styles.activityColumn}>
               <div className={styles.activityItem}>
-                {/* TODO: Fill these points (I couldn't find the info for them and maybe add the circles (I was having issues. Sorry) */}
-                <div className={styles.activityNumber}>1.</div>
-                <p className={styles.activityText}>Add something here.</p>
+                <div className="h-8 w-8 sm:h-12 sm:w-12 rounded-full bg-yellow-100 flex items-center justify-center">
+                  <p>1</p>
+                </div>
+                <div className={styles.activityTextWrapper}>
+                  <h1 className="text-lg font-bold">Add something here</h1>
+                  <p>Add something here.</p>
+                </div>
               </div>
               <div className={styles.activityItem}>
-                <div className={styles.activityNumber}>2.</div>
-                <p className={styles.activityText}>Add something here.</p>
+                <div className="h-8 w-8 sm:h-12 sm:w-12 rounded-full bg-yellow-100 flex items-center justify-center">
+                  <p>2</p>
+                </div>
+                <div className={styles.activityTextWrapper}>
+                  <h1 className="text-lg font-bold">Add something here</h1>
+                  <p>Add something here.</p>
+                </div>
               </div>
               <div className={styles.activityItem}>
-                <div className={styles.activityNumber}>3.</div>
-                <p className={styles.activityText}>Add something here.</p>
+                <div className="h-8 w-8 sm:h-12 sm:w-12 rounded-full bg-yellow-100 flex items-center justify-center">
+                  <p>3</p>
+                </div>
+                <div className={styles.activityTextWrapper}>
+                  <h1 className="text-lg font-bold">Add something here</h1>
+                  <p>Add something here.</p>
+                </div>
               </div>
               <div className={styles.activityItem}>
-                <div className={styles.activityNumber}>4.</div>
-                <p className={styles.activityText}>Add something here.</p>
+                <div className="h-8 w-8 sm:h-12 sm:w-12 rounded-full bg-yellow-100 flex items-center justify-center">
+                  <p>4</p>
+                </div>
+                <div className={styles.activityTextWrapper}>
+                  <h1 className="text-lg font-bold">Add something here</h1>
+                  <p>Add something here.</p>
+                </div>
               </div>
             </div>
             <div className={styles.activityColumn}>
               <div className={styles.activityItem}>
-                <div className={styles.activityNumber}>5.</div>
-                <p className={styles.activityText}>Add something here.</p>
+                <div className="h-8 w-8 sm:h-12 sm:w-12 rounded-full bg-yellow-100 flex items-center justify-center">
+                  <p>5</p>
+                </div>
+                <div className={styles.activityTextWrapper}>
+                  <h1 className="text-lg font-bold">Add something here</h1>
+                  <p>Add something here.</p>
+                </div>
               </div>
               <div className={styles.activityItem}>
-                <div className={styles.activityNumber}>6.</div>
-                <p className={styles.activityText}>Add something here.</p>
+                <div className="h-8 w-8 sm:h-12 sm:w-12 rounded-full bg-yellow-100 flex items-center justify-center">
+                  <p>6</p>
+                </div>
+                <div className={styles.activityTextWrapper}>
+                  <h1 className="text-lg font-bold">Add something here</h1>
+                  <p>Add something here.</p>
+                </div>
               </div>
               <div className={styles.activityItem}>
-                <div className={styles.activityNumber}>7.</div>
-                <p className={styles.activityText}>Add something here.</p>
+                <div className="h-8 w-8 sm:h-12 sm:w-12 rounded-full bg-yellow-100 flex items-center justify-center">
+                  <p>7</p>
+                </div>
+                <div className={styles.activityTextWrapper}>
+                  <h1 className="text-lg font-bold">Add something here</h1>
+                  <p>Add something here.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div className={styles.buttonWrapper}>
-      <button className={styles.Button} onClick={onBack}>
-          <img src="/images/backbuttonnew.svg" alt="Continue" />
+        <button className={styles.Button} onClick={onBack}>
+          <img src="/images/backbuttonnew.svg" alt="Back" />
         </button>
         <button className={styles.continueButton} onClick={onNext}>
           <img src="/images/continue.svg" alt="Continue" />
