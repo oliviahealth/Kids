247276db65aeaa99a002c3b1f17e4f743e46eff6
ayfatoye <afatoye@tamu.edu>
2024-07-09 15:57:13 -0500

    included the add-a-photo button to the key takeaway page of the relaxation activity

--- src/app/home/pregnancy/relaxation-yoga/components/Page3.tsx
+++ src/app/home/pregnancy/relaxation-yoga/components/Page3.tsx
@@ -10,29 +10,59 @@ const RelaxationYogaPage3: React.FC<{
     <>
       <div className={styles.headerWrapper}>
         <div className={styles.header}>
-          <img src="/images/flower.svg" alt="Flower Icon" className={styles.icon} />
+          <img
+            src="/images/flower.svg"
+            alt="Flower Icon"
+            className={styles.icon}
+          />
           <div>
             <h1 className={styles.title}>Relaxation Yoga</h1>
-            <h2 className={styles.subtitle}>Caregiver Wellness - Blossom Haven</h2>
+            <h2 className={styles.subtitle}>
+              Caregiver Wellness - Blossom Haven
+            </h2>
           </div>
         </div>
         <Link href="/home">
           <button className={styles.exitButton}>
-            <img src="/images/exit.svg" alt="Exit" className={styles.exitIcon} />
+            <img
+              src="/images/exit.svg"
+              alt="Exit"
+              className={styles.exitIcon}
+            />
           </button>
         </Link>
       </div>
       <div className={styles.contentWrapper}>
-        <div className={styles.content}>
-          <h3 className={styles.heading}><b>Key Takeaways</b></h3>
-          <p className={styles.text}>
-          Congrats, you have completed your Relaxation Yoga session! After completing one session, you will recieve your activity completion sticker. These are some of the many poses available for you to relax, stretch, reduce pain, and build flexibility. Don’t be afraid to venture out and find new poses. Just remember to focus on comfort and relaxation while doing so.
-          </p>
+        <div
+          className={`${styles.content} d-flex flex-column justify-content-between`}
+        >
+          <div>
+            <h3 className={styles.heading}>
+              <b>Key Takeaways</b>
+            </h3>
+            <p className={styles.text}>
+              Congrats, you have completed your Relaxation Yoga session! After
+              completing one session, you will recieve your activity completion
+              sticker. These are some of the many poses available for you to
+              relax, stretch, reduce pain, and build flexibility. Don't be
+              afraid to venture out and find new poses. Just remember to focus
+              on comfort and relaxation while doing so.
+            </p>
+          </div>
+          <div className={`pt-3 align-self-center`}>
+            <Image
+              height={250}
+              width={250}
+              priority
+              src="/images/pregnancy_add_a_photo.svg"
+              alt="Exit button"
+            />
+          </div>
         </div>
         {/* TODO: Add the photo pic under */}
       </div>
       <div className={styles.buttonWrapper}>
-      <button className={styles.Button} onClick={onBack}>
+        <button className={styles.Button} onClick={onBack}>
           <img src="/images/backbuttonnew.svg" alt="Continue" />
         </button>
       </div>
