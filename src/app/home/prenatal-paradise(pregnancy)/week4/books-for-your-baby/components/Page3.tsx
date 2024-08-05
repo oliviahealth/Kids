b014ed301dc4545fd1bb2958c2785ec604970918
Sebastian Oberg <Sebastianoberg2002@gmail.com>
2024-08-05 14:58:01 -0500

    Added the last two activities

---
+++ src/app/home/prenatal-paradise(pregnancy)/week4/books-for-your-baby/components/Page3.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import logo from "../../../../../../../public/images/dashboard/pregnancy/prenatal-icon.svg";
+import photo from "../../../../../../../public/images/pregnancy_add_a_photo.svg";
+import KeyTakeaways from "@/components/Template/KeyTakeaways";
+import Footer from "@/components/Template/Footer";
+import Header from "@/components/Template/Header";
+
+export default function Page3({
+  onNext,
+  onBack,
+}: {
+  onNext: () => void;
+  onBack: () => void;
+}) {
+  const takeawayText = `Reading to your baby in the womb promotes early bonding and stimulates cognitive development. It helps the baby become familiar with parents' voices, creating a sense of security and comfort. Additionally, this practice can instill a love for reading and learning from an early age, setting a positive foundation for future literacy skills.`;
+
+  return (
+    <>
+      <Header
+        logoSrc={logo}
+        title="Books for your Baby!"
+        subtitle="Language, Literacy, & Speech - Prenatal Paradise"
+        homeLink="/home"
+        subtitleColor="#F797FF"
+      />
+
+      <KeyTakeaways text={takeawayText} photoSrc={photo} photoAlt="Photo" />
+      <Footer
+        onNext={onNext}
+        onBack={onBack}
+        leftButtonText="Back"
+        rightButtonText="Continue"
+        leftClickAllowed={true}
+        rightHasLink={true}
+        rightLinkHref={"/home"}
+      />
+    </>
+  );
+}
