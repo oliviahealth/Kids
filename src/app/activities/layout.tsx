aa8b49e580c90c472c88b1aed7569c2d5ff2a5ec
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-03-26 13:51:40 -0500

    Implement auth

--- src/app/activities/layout.tsx
+++ src/app/activities/layout.tsx
@@ -4,10 +4,12 @@ import useAppStore from "@/lib/useAppStore";
 import { redirect } from "next/navigation";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    // const user = useAppStore(state => state.user);
-    // if(!user) {
-    //     redirect("/sign-in");
-    // }
+    const user = useAppStore(state => state.user);
+    if(!user) {
+        redirect("/sign-in");
+    }
+
+    console.log(user);
   
     // If authenticated, render the layout with the children content
     return (
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1504,11 +1504,11 @@ const mapMarkers1 = [
 ];
 
 const Dashboard: React.FC = () => {
-  // const user = useAppStore(state => state.user);
+  const user = useAppStore(state => state.user);
 
-  // if(!user) {
-  //   redirect('/sign-in')
-  // }
+  if(!user) {
+    redirect('/sign-in')
+  }
   
   const [currentMapIndex, setCurrentMapIndex] = useState(0);
   const [containerDimensions, setContainerDimensions] = useState({
