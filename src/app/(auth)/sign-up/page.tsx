a94b2081f3e97f753edb38eb6d8ad066b7797964
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-04-07 09:37:51 -0500

    Add suspense

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from "react";
+import { Suspense } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
@@ -74,7 +75,7 @@ const SignupPage: React.FC = () => {
   };
 
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       {requestAccessTokenStatus == 'success' && (<div role="alert" className="alert alert-success my-4">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -177,7 +178,7 @@ const SignupPage: React.FC = () => {
           <Link href={"/sign-in"}>Sign In</Link>
         </span>
       </p>
-    </>
+    </Suspense>
   );
 };
 
